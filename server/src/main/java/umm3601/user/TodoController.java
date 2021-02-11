336a54b8ed266b00170c7982a13d497023224f92
Noah <const112@umn.edu>
2021-02-11 13:09:08 -0600

    Todo Controller
    Added TodoController class. Very similar to UserController but handles todos.

---
+++ server/src/main/java/umm3601/user/TodoController.java
@@ -0,0 +1,31 @@
+package umm3601.user;
+
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+public class TodoController {
+
+  private Database database;
+
+  public TodoController(Database database) {
+    this.database = database;
+  }
+
+  public void getTodo (Context ctx) {
+    String id = ctx.pathParam("id", String.class).get();
+    Todo todo = database.getTodo(id);
+    if (todo != null) {
+      ctx.json(todo);
+      ctx.status(201);
+    } else {
+      throw new NotFoundResponse("No user with id " + id + " was found.");
+    }
+
+    public void getTodos (Context ctx) {
+      Todo[] todos = database.listTodos(ctx.queryParamMap());
+      ctx.json(todos);
+    }
+  }
+
+
+}
